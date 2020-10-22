ae980a596ebd7f9470ed3db63d40a29507c5f1c7
Rogelio M Corsino <rcorsino@gmail.com>
2020-10-22 15:10:50 -0300

    draw buttons

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Testing if my Script is working");
+const app = document.querySelector(".App");
+const calcKeys = [
+  ["1", "2", "3", "+"],
+  ["4", "5", "6", "-"],
+  ["7", "8", "9", "*"],
+  ["C", "0", "=", "/"],
+];
+const opertors = ["+", "-", "*", "/"];
+let outPut;
+let keys;
+let btn;
+
+document.addEventListener("DOMContentLoaded", function () {
+  outPut = document.createElement("div");
+  outPut.innerHTML = "0";
+  outPut.classList.add("OutPut");
+  app.appendChild(outPut);
+  console.log(outPut);
+
+  // Build the buttons or calcKeys..
+  // keys = document.createElement("div");
+  // keys.classList.add("Keys");
+  // app.appendChild(keys);
+
+  for (let i = 0; i < calcKeys.length; i++) {
+    let div = document.createElement("div");
+    div.classList.add("Row");
+    calcKeys[i].map((item, index) => {
+      btn = document.createElement("div");
+      btn.innerHTML = item;
+      btn.classList.add("Btn"); //this has to change
+      btn.addEventListener("click", btnHit);
+      div.appendChild(btn);
+    });
+    app.appendChild(div);
+  }
+  function btnHit() {
+    console.log("You Clicked a button!!!");
+  }
+});
