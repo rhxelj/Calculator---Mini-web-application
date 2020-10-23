1854a2609db81d838bc84317ed55f45804be128e
Rogelio M Corsino <rcorsino@gmail.com>
2020-10-23 20:31:27 -0300

    finish all buttons actions

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const calcKeys = [
   ["7", "8", "9", "*"],
   ["C", "0", "=", "/"],
 ];
-const opertors = ["+", "-", "*", "/"];
+const oper = ["+", "-", "*", "/"];
 let outPut;
 let keys;
 let btn;
@@ -34,7 +34,43 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     app.appendChild(div);
   }
-  function btnHit() {
-    console.log("You Clicked a button!!!");
+  function btnHit(event) {
+    let myValue = this.innerText;
+    let myCal = outPut.innerText;
+    if (myCal == "0") {
+      myCal = "";
+    }
+
+    if (myValue == "=") {
+      myCal = eval(myCal);
+    } else {
+      let lastChar = myCal.substring(myCal.length - 1);
+      console.log(lastChar);
+
+      if (oper.includes(myValue)) {
+        if (oper.includes(lastChar)) {
+          myCal = myCal.substring(0, myCal.length - 1);
+        } else {
+          myCal = eval(myCal);
+        }
+      }
+      myCal = myCal + myValue;
+    }
+    if (myValue == "C") {
+      myCal = 0;
+    }
+    outPut.innerText = myCal;
+
+    // if (outPut.innerText == "0") {
+    //   outPut.innerText = "";
+    // }
+    // if (this.innerText == "C") {
+    //   outPut.innerText = "0";
+    // }
+    // if (this.innerText == "=") {
+    //   outPut.innerText = eval(outPut.innerText);
+    // } else {
+    //   outPut.innerText += this.innerText;
+    // }
   }
 });
